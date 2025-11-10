e51ed9472dcd1805de13e4406c657c95de520102
Alan Smith <commit@alanwsmith.com>
2025-11-10 18:10:34 -0500

    basics in place

--- content/scripts/page.js
+++ content/scripts/page.js
@@ -1,6 +1,17 @@
-function setProp(key, value) {
-  document.documentElement.style.setProperty(key, value);
-}
-
 export default class {
+  changeTheme(event, _el) {
+    if (event.type === "input") {
+      updateStyles(event.target.value);
+    }
+  }
+
+  initTheme(_event, el) {
+    let theme = localStorage.getItem("theme");
+    if (!theme) {
+      theme = "auto";
+    }
+    if (el.value === theme) { 
+      el.checked = true;
+    }
+  }
 };
